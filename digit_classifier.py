b71e6d4220594ddbf66a9af8043f020935c75616
Adarsh <adarsh.bhaskar7903@gmail.com>
2024-05-22 21:27:43 +0530

    Added training code for the model

--- digit_classifier.py
+++ digit_classifier.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torchtyping import TensorType
+import matplotlib.pyplot as plt
 
 class Solution(nn.Module):
     '''
@@ -25,4 +26,35 @@ class Solution(nn.Module):
         '''
         torch.manual_seed(0)
         # Return the model's prediction in 4 decimal places
-        return torch.round(self.sigmoid(self.final_layer(self.dropout(self.relu(self.first_layer(images))))), decimals=4)    
+        out = torch.round(self.sigmoid(self.final_layer(self.dropout(self.relu(self.first_layer(images))))), decimals=4)
+        return out
+
+
+model = Solution()
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+epochs = 5
+for epoch in range(epochs):
+    for images, labels in train_dataloader:
+        images = images.view(images.shape[0], 784)
+
+        # Training body
+        model_prediction = model(images)
+        optimizer.zero_grad()
+        loss = loss_function(model_prediction, labels)
+        loss.backward() # To calculate the derivates of all the weights
+        optimizer.step() # new_w = old_w - derivative * learning_rate
+
+model.eval()
+for images, labels in test_dataloader:
+    images = images.view(images.shape[0], 784)
+
+    model_prediction = model(images)
+    max, idx = torch.max(model_prediction, dim=1)
+    for i in range(len(images)):
+        plt.imshow(images[i].view(28, 28))
+        plt.show()
+        print(idx[i].item())
+    break
